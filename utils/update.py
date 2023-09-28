89bbd137469a46ff80a84969e0b022895f049380
r4ulcl <raulcalvolaorden@gmail.com>
2023-09-28 16:58:03 +0100

    Fix tests and Flake8

--- utils/update.py
+++ utils/update.py
@@ -4,14 +4,17 @@ import subprocess
 import requests
 import re
 
+
 def is_git_installed():
     try:
-        subprocess.run(["git", "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        subprocess.run(["git", "--version"], stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE, check=True)
         return True
     except FileNotFoundError:
         return False
     except subprocess.CalledProcessError:
         return False
+
 
 def get_latest_github_release(repo_url):
     try:
@@ -22,35 +25,45 @@ def get_latest_github_release(repo_url):
         else:
             return None
     except Exception as e:
+        print(e)
         return None
+
 
 def check_for_update(VERSION):
     repo_url = "https://api.github.com/repos/r4ulcl/wifi_db"
     script_path = os.path.abspath(__file__)
     script_dir = os.path.dirname(script_path)
-    
+
     if not is_git_installed():
         print("Git is not installed on your system. Please install Git.")
         sys.exit(1)
-    
+
     latest_release_tag = get_latest_github_release(repo_url)
-    
+
     if latest_release_tag:
         # Get only the number part, without v and -dev
-        latest_release_tag_number = re.search(r'(\d+(\.\d+)+)', latest_release_tag).group(1)
+        latest_release_tag_number = re.search(r'(\d+(\.\d+)+)',
+                                              latest_release_tag).group(1)
         current_number = re.search(r'(\d+(\.\d+)+)', VERSION).group(1)
-        #print(latest_release_tag_number)
-        #print(current_number)
+        # print(latest_release_tag_number)
+        # print(current_number)
         if latest_release_tag_number > current_number:
-            user_choice = input("A new version is available (v"+latest_release_tag_number+"). Do you want to update (Y/n)?: ").strip().lower() or "y"
+            user_choice = input("A new version is available (v" +
+                                latest_release_tag_number +
+                                "). Do you want to update (Y/n)?: "
+                                ).strip().lower() or "y"
             if user_choice in ("", "y", "Y"):
                 print("Updating...")
-                update_process = subprocess.Popen(["git", "pull"], cwd=script_dir)
-                update_process.wait()  # Wait for the Git pull operation to complete
+                update_process = subprocess.Popen(["git", "pull"],
+                                                  cwd=script_dir)
+                # Wait for the Git pull operation to complete
+                update_process.wait()
                 print("Update complete. Please run again the script.")
                 sys.exit()
             else:
                 print("You chose not to update. Running the current version.")
+        elif latest_release_tag_number < current_number:
+            print("You are using a future version ;) ("+VERSION+").\n")
         else:
             print("You are using the latest version ("+VERSION+").\n")
     else:
