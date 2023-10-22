8616a082e3a87f92376782055641226b15c009ff
r4ulcl <raulcalvolaorden@gmail.com>
2023-10-22 14:53:34 +0200

    Fix double subprocess

--- utils/wifi_db_aircrack.py
+++ utils/wifi_db_aircrack.py
@@ -656,8 +656,6 @@ def exec_hcxpcapngtool(name, database, verbose):
         errors = 0
         fileName = name
         # exec_hcxpcapngtool
-        arguments = " --all " + fileName + ' -o test.22000'
-        subprocess.call(["hcxpcapngtool", arguments])
         execute_process = subprocess.Popen(["hcxpcapngtool", "--all", fileName, "-o", "test.22000"])
         execute_process.wait()  # Wait for the installation process to complete
         # Read output (fileName) each line
